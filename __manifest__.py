bd6f5b9e2158cd9498f57c2e4af410798f1aec7e
Diego <Diego@DESKTOP-C0319P1>
2025-12-11 11:24:08 -0600

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Hidden info",
+    "summary": "Hiding information on receipts",
+    "description": """Hiding information on receipts""",
+    "author": "DGV",
+    # 'website': "https://www.yourcompany.com",
+    "category": "Point of Sale",
+    "version": "0.1",
+    "depends": ["point_of_sale"],
+    "assets": {
+        "point_of_sale._assets_pos": [
+            "hidden_info/static/src/xml/order_receipt_inherit.xml",
+        ],
+    },
+}
